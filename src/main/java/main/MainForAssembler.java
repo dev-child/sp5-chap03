f2bb121f21f5adb52555b7fbc8be7d2eb0e436b6
dev-child <dev.child.se@gmail.com>
2024-03-10 00:51:30 +0900

    24.3.12

---
+++ src/main/java/main/MainForAssembler.java
@@ -0,0 +1,59 @@
+package main;
+
+import assembler.Assembler;
+import spring.Member;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainForAssembler {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("명령어를 입력하세요:");
+            String command = reader.readLine();
+            if (command.equalsIgnoreCase("exit")) {
+                System.out.println("종료합니다.");
+                break;
+            }
+            if (command.startsWith("new ")) {
+                processNewCommand(command.split(" "));
+                continue;
+            } else if (command.startsWith("chage ")) {
+                processChangeCommand(command.split(" "));
+                continue;
+            }
+            printHelp();
+        }
+    }
+
+    private static Assembler assembler = new Assembler();
+
+    private static void processNewCommand(String[] arg) {
+        if (arg.length != 5) {
+            printHelp();
+            return;
+        }
+        MemberRegisterService regSvc = assembler.getMemberRegisterService();
+        RegisterRequest req = new RegisterRequest();
+        req.setEmail(arg[1]);
+        req.setName(arg[2]);
+        req.setPassword(arg[3]);
+        req.setConfirmPassword(arg[4]);
+
+        if (!req.is)
+    }
+
+    private static void processChangeCommand(String[] arg) {
+
+    }
+
+    private static void printHelp() {
+    }
+
+
+}

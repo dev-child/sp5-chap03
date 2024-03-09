428c54ff19b9549b5924feefebc7beffd36c0682
dev-child <dev.child.se@gmail.com>
2024-03-09 21:45:43 +0900

    first

---
+++ src/main/java/com/example/sp5chap03/Sp5Chap03Application.java
@@ -0,0 +1,13 @@
+package com.example.sp5chap03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp5Chap03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp5Chap03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sp5chap03/Sp5Chap03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sp5chap03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp5Chap03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
